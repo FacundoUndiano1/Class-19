6c5b3e03780a7ff903e6817a68e7a56992708fc2
Facundo Undiano <facundoundiano@gmail.com>
2022-11-09 18:09:48 -0300

    updating files

--- src/routers/mainRouter.js
+++ src/routers/mainRouter.js
@@ -1,10 +1,23 @@
+
 const express = require('express');
-const router = express.Router()
+const router = express.Router();
 const mainController = require('../controllers/mainController');
 
 
 router.use('/home', mainController.home);
 
 router.use('/about', mainController.about);
+
+//Parameterized route to products
+router.use('/products/:idProduct', mainController.products);
+
+//Parameterized route to channels storing users search
+router.use('/channels/:idChannel', mainController.channels);
+
+router.use('/episodes/:idEpisode/comments', mainController.episodes);
+
+router.use('/series/:idSerie/comments/:idComment?', mainController.series);
+
+router.use('/movies/:idMovie/comments/:idComment?', mainController.movies);
 
 module.exports = router;
