fcd5fb72d8f6fa7bab62f95d798c13b87fe6c24e
Facundo Undiano <facundoundiano@gmail.com>
2022-11-11 14:33:00 -0300

    small changes

--- src/app.js
+++ src/app.js
@@ -4,13 +4,15 @@ const app = express();
 const path = require('path');
 const mainRouter = require('./routers/mainRouter');
 
-let PORT = process.env.PORT || 3030;
+let PORT = process.env.PORT || 3000;
 app.listen(PORT, (req, res) => {
   console.log('Servidor corriendo en puerto ' + PORT);
 })
 
-app.use(express.static(path.resolve(__dirname, './public')));
+app.use(express.static(path.resolve(__dirname, '../public')));
+app.set('views', path.resolve(__dirname, 'views'));
+app.set('view engine', 'ejs');
 
 app.use('/', mainRouter); //All routes starting with / are going to be answered by mainRouter
 
-app.set('view engine', 'ejs');
+
--- src/controllers/mainController.js
+++ src/controllers/mainController.js
@@ -5,11 +5,11 @@ const path = require('path');
 const mainRouter = {
 
   home: (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../views/home.html'));
+    res.render('home');
   },
 
   about: (req, res) => {
-    res.sendFile(path.resolve(__dirname, '../views/about.html'));
+    res.render('about');
   },
 
   products: (req, res) => {
